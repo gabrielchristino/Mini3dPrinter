3bcbe228ac31f4a9fc7088b3821f3b901b73105a
Gabriel <gabriel.christino@gmail.com>
2022-02-16 16:46:33 -0300

    controller

---
+++ 3dMiniPrint/Form1.Designer.vb
@@ -0,0 +1,116 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(318, 353)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Send"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.TextBox1.Location = New System.Drawing.Point(12, 41)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.PlaceholderText = "Insert GCODE here to send"
+        Me.TextBox1.Size = New System.Drawing.Size(381, 306)
+        Me.TextBox1.TabIndex = 1
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(102, 12)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(210, 23)
+        Me.ComboBox1.TabIndex = 2
+        Me.ComboBox1.UseWaitCursor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(318, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Disconnect"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 12)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(84, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Refresh ports"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.TextBox2.Location = New System.Drawing.Point(399, 41)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox2.Size = New System.Drawing.Size(381, 306)
+        Me.TextBox2.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(803, 382)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.MaximumSize = New System.Drawing.Size(819, 421)
+        Me.MinimumSize = New System.Drawing.Size(819, 421)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ 3dMiniPrint/Form1.vb
@@ -0,0 +1,69 @@
+﻿Imports System.Threading
+
+Imports System.IO
+
+Imports System.Text
+
+Imports System.IO.Ports
+Public Class Form1
+
+    Public lineToSend As Integer = -1
+    Public sendLine As Boolean = False
+
+    Public Event SerialDataRecieved(ByVal dataStr As String)
+    WithEvents comPort As SerialPort = New SerialPort()
+
+    Delegate Sub AddText(ByVal Text As String)
+    Private Sub comPort_DataReceived(ByVal sender As Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles comPort.DataReceived
+        RaiseEvent SerialDataRecieved(comPort.ReadLine())
+    End Sub
+
+    Private Sub HandleData(ByVal dataStr As String) Handles Me.SerialDataRecieved
+        TextBox2.Invoke(New AddText(AddressOf TextBox2.AppendText), dataStr + vbCrLf)
+
+        If (dataStr.Contains(">") And TextBox1.Lines.Length - 1 > lineToSend And sendLine) Then
+            lineToSend += 1
+            comPort.Write(TextBox1.Lines(lineToSend) + vbCrLf)
+        ElseIf (TextBox1.Lines.Length - 1 = lineToSend) Then
+            sendLine = False
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        loadComPorts()
+    End Sub
+
+    Private Sub loadComPorts()
+        Dim AvailablePorts() As String = SerialPort.GetPortNames()
+
+        Dim Port As String
+
+        ComboBox1.Items.Clear()
+
+        For Each Port In AvailablePorts
+            ComboBox1.Items.Add(Port)
+        Next Port
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'For Each strLine As String In TextBox1.Text.Split(vbNewLine)
+        lineToSend = 0
+        sendLine = True
+        comPort.Write(TextBox1.Lines(lineToSend) + vbCrLf)
+        'Next
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        comPort.PortName = ComboBox1.SelectedItem   'Assign the port name to the MyCOMPort object
+        comPort.BaudRate = 9600      'Assign the Baudrate to the MyCOMPort object
+        comPort.Parity = Parity.None   'Parity bits = none  
+        comPort.DataBits = 8             'No of Data bits = 8
+        comPort.StopBits = StopBits.One  'No of Stop bits = 1
+        comPort.Open()
+        comPort.Write(vbCrLf)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If (comPort.IsOpen) Then comPort.Close()
+    End Sub
+End Class
